14fbb6276792316ea1c96dcbf6bb3fafa470041b
jersonb <jersonlbrito@hotmail.com>
2022-03-05 15:20:29 -0300

    adding project domain

---
+++ src/Olinda.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Olinda.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Olinda.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Olinda.Domain;
+public class Class1
+{
+
+}
